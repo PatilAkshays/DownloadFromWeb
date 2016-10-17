71e9c041ef6eabf65983bb7a430de3b3943ccefb
Akshay Patil <patilakshays1994@gmail.com>
2016-10-17 12:31:12 +0530

    Modified UI

--- APDownloadFromWeb/ViewController.h
+++ APDownloadFromWeb/ViewController.h
@@ -10,10 +10,10 @@
 #define kFirstImageURLString @"http://images5.fanpop.com/image/photos/31500000/Doremon-doremon-31525826-454-500.jpg"
 
 
+#define kSecondImageURLString @"https://shechive.files.wordpress.com/2012/06/cute-cartoons-21.jpg"
 
-//#define kSecondImageURLString @"https://www.google.co.in/search?q=famous+cartoon+image&biw=1074&bih=633&tbm=isch&imgil=Bzv9UpYiFnt7MM%253A%253BZOKUd3xaIbGsfM%253Bhttp%25253A%25252F%25252Fblog.pakistani.pk%25252Ffamous-cartoon-characters%25252F&source=iu&pf=m&fir=Bzv9UpYiFnt7MM%253A%252CZOKUd3xaIbGsfM%252C_&usg=__tJHjgt1ZVZ4lKOaE6U2eoBsn9m4%3D&ved=0ahUKEwjJgILRi9_PAhUNUI8KHclfBfIQyjcIOg&ei=D1MDWIm9A42gvQTJv5WQDw#imgrc=Bzv9UpYiFnt7MM%3A"
+#define kThirdImageURLString @"http://kingofwallpapers.com/cartoon-picture/cartoon-picture-010.jpg"
 
-//#define kThirdImageURLString @"https://www.google.co.in/search?q=famous+cartoon+image&biw=1074&bih=633&tbm=isch&imgil=EKmwvBCa9SJErM%253A%253BZOKUd3xaIbGsfM%253Bhttp%25253A%25252F%25252Fblog.pakistani.pk%25252Ffamous-cartoon-characters%25252F&source=iu&pf=m&fir=EKmwvBCa9SJErM%253A%252CZOKUd3xaIbGsfM%252C_&usg=__69_X1bGZgFLeO4oHeudMMK0Ydno%3D&ved=0ahUKEwjJgILRi9_PAhUNUI8KHclfBfIQyjcIOg&ei=D1MDWIm9A42gvQTJv5WQDw#imgrc=EKmwvBCa9SJErM%3A"
 #import <UIKit/UIKit.h>
 
 
@@ -25,9 +25,13 @@
 
 @property (strong, nonatomic) IBOutlet UIActivityIndicatorView *myFirstImageDownloadIndicator;
 
+@property (strong, nonatomic) IBOutlet UIImageView *mySecondImageView;
 
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *mySecondImageDownloadIndicator;
 
+@property (strong, nonatomic) IBOutlet UIImageView *myThirdImageView;
 
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *myThirdImageDownloadIndicator;
 
 
 @end
--- APDownloadFromWeb/ViewController.m
+++ APDownloadFromWeb/ViewController.m
@@ -29,11 +29,77 @@
 -(void)setUp {
     
     self.myFirstImageDownloadIndicator.hidesWhenStopped = YES;
+    
+    self.mySecondImageDownloadIndicator.hidesWhenStopped = YES;
+    
+    self.myThirdImageDownloadIndicator.hidesWhenStopped = YES;
+    
 }
 
 -(void)downloadFirstImageFromURLString: (NSString *)urlString {
     
     [self.myFirstImageDownloadIndicator startAnimating];
+    
+    //nsurl create
+    
+    NSURL *URL = [NSURL URLWithString:urlString];
+    
+    //session configuration create
+    
+    NSURLSessionConfiguration *myConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    //session alloc init
+    
+    NSURLSession *mySession = [NSURLSession sessionWithConfiguration:myConfiguration];
+    
+    //download task alloc init
+    
+    NSURLSessionDownloadTask *myDownloadTask = [mySession downloadTaskWithURL:URL completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+       
+        if (error) {
+            
+            //alert
+        }
+        else {
+            
+            if (response) {
+                
+                NSHTTPURLResponse *httpResponce = (NSHTTPURLResponse *) response;
+                
+                if (httpResponce.statusCode == 200) {
+                    
+                    NSData *imageData = [NSData dataWithContentsOfURL:location];
+                    
+                    UIImage *myImage = [UIImage imageWithData:imageData];
+                    
+                    [self performSelectorOnMainThread:@selector(reloadImageFirst:) withObject:myImage waitUntilDone:NO];
+                }
+                
+                else{
+                    //alert http status code
+                    
+                    [self.myFirstImageDownloadIndicator stopAnimating];
+                }
+            }
+            
+            else{
+                //alerrt no responce
+                
+                [self.myFirstImageDownloadIndicator stopAnimating];
+
+            }
+        }
+        
+    }];
+    //resume Download task
+    [myDownloadTask resume];
+    
+}
+
+-(void)downloadSecondImageFromURLString: (NSString *)urlString {
+    
+    
+    [self.mySecondImageDownloadIndicator startAnimating];
     
     //nsurl create
     
@@ -72,7 +138,7 @@
                     
                     UIImage *myImage = [UIImage imageWithData:imageData];
                     
-                    [self performSelectorOnMainThread:@selector(reloadImage:) withObject:myImage waitUntilDone:NO];
+                    [self performSelectorOnMainThread:@selector(reloadImageSecond:) withObject:myImage waitUntilDone:NO];
                 
                     
                 }
@@ -80,13 +146,13 @@
                     
                     //alert http status code
                     
-                    [self.myFirstImageDownloadIndicator stopAnimating];
+                    [self.mySecondImageDownloadIndicator stopAnimating];
                 }
             }
             else{
                 
                 //alert no responce
-                [self.myFirstImageDownloadIndicator stopAnimating];
+                [self.mySecondImageDownloadIndicator stopAnimating];
                 
             }
         }
@@ -98,17 +164,98 @@
     
     [myDownloadTask resume];
 }
+-(void)downloadThirdImageFromURLString: (NSString *)urlString {
+    
+    [self.myThirdImageDownloadIndicator startAnimating];
+    
+    //nsurl create
+    
+    NSURL *URL = [NSURL URLWithString:urlString];
+    
+    //session configuration create
+    
+    NSURLSessionConfiguration *myConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    //session alloc init
+    
+    NSURLSession *mySession = [NSURLSession sessionWithConfiguration:myConfiguration];
+    
+    //download task alloc init
+    
+    NSURLSessionDownloadTask *myDownloadTask = [mySession downloadTaskWithURL:URL completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        if (error) {
+            
+            //alert
+        }
+        else {
+            
+            if (response) {
+                
+                NSHTTPURLResponse *httpResponce = (NSHTTPURLResponse *) response;
+                
+                if (httpResponce.statusCode == 200) {
+                    
+                    NSData *imageData = [NSData dataWithContentsOfURL:location];
+                    
+                    UIImage *myImage = [UIImage imageWithData:imageData];
+                    
+                    [self performSelectorOnMainThread:@selector(reloadImageThird:) withObject:myImage waitUntilDone:NO];
+                }
+                
+                else{
+                    //alert http status code
+                    
+                    [self.myThirdImageDownloadIndicator stopAnimating];
+                }
+            }
+            
+            else{
+                //alerrt no responce
+                
+                [self.myThirdImageDownloadIndicator stopAnimating];
+                
+            }
+        }
+        
+    }];
+    //resume Download task
+    [myDownloadTask resume];
+    
+}
 
--(void)reloadImage:(UIImage *)myImage{
+-(void)reloadImageFirst:(UIImage *)myImage{
     
     [self.myFirstImageDownloadIndicator stopAnimating];
     
+
     self.myFirstImageView.image = myImage;
+    
+    
+}
+-(void)reloadImageSecond:(UIImage *)myImage{
+   
+    [self.mySecondImageDownloadIndicator stopAnimating];
+
+    self.mySecondImageView.image = myImage;
+
+
 }
 
+-(void)reloadImageThird:(UIImage *)myImage{
 
+    [self.myThirdImageDownloadIndicator stopAnimating];
+
+    
+    self.myThirdImageView.image = myImage;
+    
+}
 - (IBAction)startDowloadButton:(id)sender {
     
     [self downloadFirstImageFromURLString:kFirstImageURLString];
+    
+    [self downloadSecondImageFromURLString:kSecondImageURLString];
+    
+    [self downloadThirdImageFromURLString:kThirdImageURLString];
 }
 @end
